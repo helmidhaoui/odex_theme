f470bee47faddf7e03bf4138f991a126f3455fa3
unknown <helmi.dhaoui@gmail.com>
2018-09-25 13:59:54 +0100

    first commit

---
+++ enterprise_theme/controllers/change_color.py
@@ -0,0 +1,47 @@
+    #-*- coding: utf-8 -*-
+from odoo import http, _
+import logging
+import random
+import json
+import os
+from odoo.http import request
+import requests
+_logger = logging.getLogger(__name__)
+less_url = 'C:\\Users\\Helmi\\dev\\workspace\\v11_modules\\ODEX-PLATFORM\\EXTRA\\enterprise_theme\\static\\src\\less\\variables.less'
+
+class ChangeColor(http.Controller):
+    
+    
+            
+    @http.route('/change_color', type='http', auth='public')
+    def change_Color(self,**post):
+        res=[]
+        color = post.get('color')
+        if os.name == 'nt':
+                less_path = repr(__file__).replace('\\controllers\\change_color.py','\\static\\src\\less\\variables.less')
+        else:
+            less_path = repr(__file__).replace('\\controllers\\change_color.py','\\static\\src\\less\\variables.less')
+        
+        if color: 
+            with open(less_url, 'r') as file:
+                data = file.readlines()
+            print ("color1 " , data[16])
+            print ("color2 " , data[18])
+            # now change the 2nd line, note that you have to add a newline
+            data[16] = '@brand-primary:            %s;\n'%color
+            data[18] = '@brand-info:            %s;\n'%color
+            # and write everything back
+            with open(less_url, 'w') as file:
+                file.writelines( data )
+                res.append({'res':1})
+        else:
+            res.append({'res':0})
+        return http.request.make_response(json.dumps(res,{
+            'Cache-Control': 'no-cache', 
+            'Content-Type': 'JSON; charset=utf-8',
+            'Access-Control-Allow-Origin':  '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, X-Requested-With',
+
+            })) 
+    
