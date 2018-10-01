27d733d7ae4b403904cceca4f4d0b6ff898afb74
unknown <helmi.dhaoui@gmail.com>
2018-10-01 14:56:42 +0100

    e

--- enterprise_theme/controllers/change_color.py
+++ enterprise_theme/controllers/change_color.py
@@ -15,8 +15,6 @@ class ChangeColor(http.Controller):
     def change_Color(self,**post):
         res=[]
         color = post.get('color')
-        _logger.warning('---------------------color (%s).', color)
-        print('-------------color',color)
         if os.name == 'nt':
             less_path = repr(__file__).replace('controllers','static')
             less_path = less_path.replace('change_color.py','src\\less\\variables.less')
@@ -26,15 +24,11 @@ class ChangeColor(http.Controller):
         else:
             less_path = repr(__file__).replace('/controllers/change_color.py','/static/src/less/variables.less')
             less_path = less_path.replace("'","")
-        _logger.warning('---------------------less_path (%s).', less_path)
         if color: 
             
             with open(less_path, 'r') as file:
                 data = file.readlines()
                 file.close()
-            _logger.warning('---------------------data (%s).', data[16])
-            print ("color1 " , data[16])
-            print ("color2 " , data[18])
             # now change the 2nd line, note that you have to add a newline
             data[16] = '@brand-primary:            %s;\n'%color
             data[18] = '@brand-info:            %s;\n'%color
@@ -44,15 +38,12 @@ class ChangeColor(http.Controller):
                 
             except Exception as e:
                 _logger.warning('---------------------e (%s).', e)
-            _logger.warning('---------------------file (%s).', file)
             with open(less_path, 'w') as file:
-                _logger.warning('---------------------data (%s).', data[18])
                 file.writelines( data )
                 file.close()
                 res.append({'res':1})
         else:
             res.append({'res':0})
-        _logger.warning('---------------------res (%s).', res)
         return http.request.make_response(json.dumps(res)) 
     
     
